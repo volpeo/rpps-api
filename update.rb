9ae46f78942d2161cf6cd82162c31fa23ceb9768
Jeremie <jeremie.bonal@gmail.com>
2016-02-03 01:23:27 +0100

    Tryna reduce memory usage.

--- update.rb
+++ update.rb
@@ -15,27 +15,19 @@ def refresh_ids(current)
   return current if current[:version] == version
 
   csv_file = []
-  puts "Fetching file from national database."
   Zip::Archive.open_buffer(open(url).read) do |archive|
-    puts "Unzipping file..."
     archive.map do |entry|
       csv_file << entry.read
     end
   end
 
-  # puts "Formatting text before parsing..."
-
-  puts "Parsing resulting CSV..."
   identifiants = csv_file[0].tr(";",",")
                         .split("\n")
                         .map {|e|
                           [e.split(",")[8].tr("\",", ""), e.split(",")[1]]
                           }
-                        .select { |e| e[0]=="Pharmacien"}
+                        .select { |e| e[0] == :Pharmacien}
                         .map { |e| e[1].tr("\",", "").to_i }
-
-  puts "#{identifiants.length} pharmacists loaded."
-  puts "Returning data as Hash."
   return {
           version: version,
           ids: identifiants
