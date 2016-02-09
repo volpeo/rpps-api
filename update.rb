a161560b1469bb29ed018ec7122304c31e4df5f4
Jeremie <jeremie.bonal@gmail.com>
2016-02-09 18:02:48 +0100

    Tried better again.

--- update.rb
+++ update.rb
@@ -21,7 +21,7 @@ def refresh_ids(current)
   puts "DBv#{version} fetched !"
 
 
-  Zip::Archive.open_buffer(zipfile) do |archive|
+  Zip::Archive.open_buffer('/tmp/zip') do |archive|
     puts "Unzipping..."
     archive.map do |entry|
       return {
