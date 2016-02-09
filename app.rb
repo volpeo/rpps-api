43035b987705c5943eba36c5eb9d31c7a323daa0
Jeremie <jeremie.bonal@gmail.com>
2016-02-09 18:40:19 +0100

    Constant became dynamic.

--- app.rb
+++ app.rb
@@ -1,19 +1,19 @@
 require_relative 'update'
 require 'sinatra'
 
-API = {version: "0000", ids:[]}
+Api = {version: "0000", ids:[]}
 
 def update
-  API = refresh_ids(API)
+  Api = refresh_ids(Api)
   puts "DB refreshed !"
 end
 
 get '/' do
   content_type :json
-  API.to_json
+  Api.to_json
 end
 
 get '/:rpps' do
   content_type :json
-  API[:ids].include?(params[:rpps].to_i).to_json
+  Api[:ids].include?(params[:rpps].to_i).to_json
 end
