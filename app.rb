f06fc865ff8a46f8c7c77af01663feb964b998e2
Jeremie <jeremie.bonal@gmail.com>
2016-02-09 18:41:22 +0100

    Alright, I got it, Ruby.

--- app.rb
+++ app.rb
@@ -1,19 +1,19 @@
 require_relative 'update'
 require 'sinatra'
 
-Api = {version: "0000", ids:[]}
+api = {version: "0000", ids:[]}
 
 def update
-  Api = refresh_ids(Api)
+  api = refresh_ids(api)
   puts "DB refreshed !"
 end
 
 get '/' do
   content_type :json
-  Api.to_json
+  api.to_json
 end
 
 get '/:rpps' do
   content_type :json
-  Api[:ids].include?(params[:rpps].to_i).to_json
+  api[:ids].include?(params[:rpps].to_i).to_json
 end
