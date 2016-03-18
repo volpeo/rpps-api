74c54a19bc716ce3e68cbc6f174c38399f670bc9
Jeremie <jeremie.bonal@gmail.com>
2016-03-18 19:56:13 +0100

    Massive overhaul from almost scratch.

--- app.rb
+++ app.rb
@@ -1,21 +1,64 @@
 require_relative 'update'
 require 'sinatra'
 
-$api = {version: "0000", ids:[]}
+$api = {version: "0000", data:[]}
 
 def update
   $api = refresh_ids($api)
   puts "DB refreshed !"
+  halt 500 if $api[:version] == "0000"
 end
 
 get '/' do
   update
-  content_type :json
-  $api.to_json
+  @version = $api[:version]
+  erb :index
 end
 
-get '/:rpps' do
-  update
+get '/entries' do
   content_type :json
-  $api[:ids].include?(params[:rpps].to_i).to_json
+  update
+  data = $api[:data]
+  data = filtered_data(data, params["column"])
+  data = paginated_data(data, params["page"])
+  data.to_json
+end
+
+get "/entries/all" do
+  update
+  filtered_data($api[:data], params["column"]).to_json
+end
+
+get '/entries/:rpps' do
+  content_type :json
+  update
+  entry = $api[:data].find{ |e| e[:rpps_id] == params[:rpps] }
+  if entry.nil?
+    halt 404
+  else
+    entry.to_json
+  end
+end
+
+def paginated_data(data, page)
+  puts "Paginating response."
+  if params["page"].nil?
+    start = 0
+  else
+    start = params["page"].to_i*50
+  end
+
+  return data[start...start+50] if start < data.length
+  []
+end
+
+def filtered_data(data, column)
+  puts "Filtering response."
+  if column.nil? || !data[0].keys.include?(column.to_sym)
+    filtered = data.map { |e| e[:rpps_id] }
+  else
+    filtered = data.map { |e| {rpps_id: e[:rpps_id], (column.to_sym) => e[column.to_sym]} }.compact
+  end
+
+  filtered.compact.delete_if { |e| e.values.include?("") }
 end
--- update.rb
+++ update.rb
@@ -23,15 +23,43 @@ def refresh_ids(current)
   Zip::Archive.open('/tmp/zip') do |archive|
     puts "Unzipping..."
     archive.map do |entry|
-      return {
-          version: version,
-          ids: entry.read
-                    .split("\n")
-                    .select { |e| e.split(";")[8].tr("\";", "") == "Pharmacien"}
-                    .map { |e| e.split(";")[1].tr("\",", "").to_i }
-         }
+      content = entry.read
+      puts "Unzipped !"
+      titles = content.split("\n")[0].split(";").map { |title| title.tr("\"", "").force_encoding("utf-8") }
+      puts "Column titles extracted !"
+      puts "Preparing to work on data."
+      raw_data = content.split("\n")[1..-1].map { |e| e.split(";").map { |e| e.tr("\"", "").force_encoding("utf-8") } }
+      puts "Selecting pharmacists only..."
+      pharmacians = raw_data.select { |e| e[titles.index("Libellé profession")] == "Pharmacien" }
+      puts "Constructing JSON objects from data..."
+
+      api_data = {
+        version: version,
+        data: pharmacians.map do |p|
+          {
+            rpps_id: p[titles.index("Identifiant PP")],
+            first_name: p[titles.index("Prénom d'exercice")],
+            last_name: p[titles.index("Nom d'exercice")],
+            email_address: p[titles.index("Adresse e-mail (coord. structure)")],
+            siret: p[titles.index("Numéro SIRET site")],
+            siren: p[titles.index("Numéro SIREN site")],
+            finess: p[titles.index("Numéro FINESS site")],
+            address: {
+              number: p[titles.index("Numéro Voie (coord. structure)")],
+              repeat: p[titles.index("Indice répétition voie (coord. Structure)")],
+              street_type: p[titles.index("Libellé type de voie (coord. structure)")],
+              street_name: p[titles.index("Libellé Voie (coord. structure)")],
+              distribution: p[titles.index("Mention distribution (coord. structure)")],
+              cedex: p[titles.index("Bureau cedex (coord. structure)")],
+              zipcode: p[titles.index("Code postal (coord. structure)")],
+              city_name: p[titles.index("Libellé commune (coord. structure)")],
+              country_name: p[titles.index("Libellé pays (coord. structure)")],
+              phone_number: p[titles.index("Téléphone (coord. structure)")]
+           }
+          }
+        end
+      }
+      return api_data
     end
   end
-
-
 end
