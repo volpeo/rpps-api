c064056e472ac830d76a3aa9ba222f805e679839
Jeremie <jeremie.bonal@gmail.com>
2016-03-19 00:42:13 +0100

    Change in environments.rb to account for prod.

--- config/environments.rb
+++ config/environments.rb
@@ -1,5 +1,18 @@
-configure :production, :development do
+configure :development do
   db = URI.parse(ENV['DATABASE_URL'] || 'postgres://localhost/rpps_api_development')
+
+  ActiveRecord::Base.establish_connection(
+      :adapter => db.scheme == 'postgres' ? 'postgresql' : db.scheme,
+      :host     => db.host,
+      :username => db.user,
+      :password => db.password,
+      :database => db.path[1..-1],
+      :encoding => 'utf8'
+  )
+end
+
+configure :production do
+  db = URI.parse(ENV['DATABASE_URL'] || 'postgres://localhost/rpps_api_production')
 
   ActiveRecord::Base.establish_connection(
       :adapter => db.scheme == 'postgres' ? 'postgresql' : db.scheme,
